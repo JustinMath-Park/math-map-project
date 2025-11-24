1ee104529d1df7a1e028395c3700def56f54426a
박세준 <justinminim4@Justin-M4.local>
2025-11-24 12:55:42 +0900

    Refactor apps/level-test: Separate CSS/JS and improve UI

---
+++ serve.py
@@ -0,0 +1,20 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8000
+DIRECTORY = "apps/level-test"
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+print(f"Serving {DIRECTORY} at http://localhost:{PORT}")
+print("Press Ctrl+C to stop")
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nStopping server...")
+        httpd.server_close()
